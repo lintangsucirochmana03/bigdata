498bcf8ed2e2d60c6703f3671d8d6632791b3fb3
lintangsucirochmana03 <lintangrochmana@gmail.com>
2018-09-12 00:13:49 -0700

    coba

---
+++ praktik/minggu_ke2/calculator/dengan_variabel.py
@@ -0,0 +1,13 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> width = 20
+>>> height = 5 * 9
+>>> width * height
+900
+>>> n
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    n
+NameError: name 'n' is not defined
+>>> n
+
---
+++ praktik/minggu_ke2/calculator/floating_dan_integer.py
@@ -0,0 +1,13 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 4 * 3.75 - 1
+14.0
+>>> tax = 12.5 / 100
+>>> price = 100.50
+>>> price * tax
+12.5625
+>>> price + _
+113.0625
+>>> round (_, 2)
+113.06
+>>> 
