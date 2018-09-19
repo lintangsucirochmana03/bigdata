ec15d185430915811fecb1a4039d18e77cb03aa2
lintangsucirochmana03 <lintangrochmana@gmail.com>
2018-09-19 00:53:38 -0700

    update

---
+++ minggu-02/praktik/src/keywoorderor.py
@@ -0,0 +1,11 @@
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+	print("-- This parrot wouldn't", action, end=' ')
+	print("if you put", voltage, "volts through it.")
+	print("-- Lovely plumage, the", type)
+	print("-- It's", state, "!")
+
+
+parrot()                     # required argument missing
+parrot(voltage=5.0, 'dead')  # non-keyword argument after a keyword argument
+parrot(110, voltage=220)     # duplicate value for the same argument
+parrot(actor='John Cleese')  # unknown keyword argument
