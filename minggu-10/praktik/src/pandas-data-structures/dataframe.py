4387480b1eff16ef4beff2bd2bb2e2e00fca3812
lintangsucirochmana03 <lintangrochmana@gmail.com>
2018-11-20 23:02:56 -0800

    update

---
+++ minggu-10/praktik/src/pandas-data-structures/dataframe.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+import pandas as pd
+
+d = {'one' : pd.Series([1., 2., 3.], index=['a', 'b', 'c']),
+	'two' : pd.Series([1., 2., 3., 4.], index=['a', 'b', 'c', 'd'])}
+
+df = pd.DataFrame(d)
+df
